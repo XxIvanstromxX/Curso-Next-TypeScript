46f8b34d8e13ad4208bab3023d660607643c24f1
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2026-04-22 22:24:38 -0600

    feat: agregar tipo Project en client.type.ts para definir estructura de proyectos

--- clientflow-ts/src/types/client.type.ts
+++ clientflow-ts/src/types/client.type.ts
@@ -6,3 +6,27 @@ export type Client = {
   company: string;
   status: string;
 };
+
+export type Project = {
+  id?: number;
+  name: string;
+  description: string;
+  status: string;
+  clientId: number;
+};
+
+// type ClientFormData = {
+//   name: string;
+//   email: string;
+//   phone: string | null;
+//   company: string;
+//   status: string;
+// };
+
+// interface ClientFormData {
+//   name: string;
+//   email: string;
+//   phone: string | null;
+//   company: string;
+//   status: string;
+// }
